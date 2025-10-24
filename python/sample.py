b2a8cfc5443b904aaaf0dca7dc1f118b2bbf2fd0
gladis-esther <gladisestheryalla@gmail.com>
2025-10-24 12:32:30 +0530

    Add files via upload

---
+++ python/sample.py
@@ -0,0 +1,12 @@
+mylist = [7, 18, 17, 45, 33, 93]
+print(mylist[-1])
+print(mylist)
+# specific range of elements
+print(mylist[1:4])
+print(mylist[:6])
+print(mylist[:])
+print(mylist[2:])
+print(mylist[-4:-1])
+print(mylist[:-1])
+print(mylist[-4:])
+print(18 in mylist)
